256f423d6d64377d133e6b5ddb83a6ceac08e06b
gerhard <gerhard.faber@gmx.de>
2017-11-29 20:49:52 +0100

    initial checkin

---
+++ coding/simplecalc/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace af
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // erste Zahl eingeben und anzeigen
+            Console.WriteLine("Bitte geben sie eine Zahl ein!");
+            string s = Console.ReadLine();
+            Console.WriteLine("Die erste Zahl ist " + s);
+
+            // Rechenoperator abfragen
+            Console.WriteLine("Bitte geben sie die gewünschte Rechenart ein: ( + - * :) ");
+            string Rechenzeichen = Console.ReadLine();
+            Console.WriteLine("Das Rechenzeichen ist : "+ Rechenzeichen);
+
+            // zweite Zahl eingeben und anzeigen
+            Console.WriteLine("Bitte geben sie eine weitere  Zahl ein!");
+            string b = Console.ReadLine();
+            Console.WriteLine("Die zweite Zahl ist " + b);
+
+            int si = Convert.ToInt32(s);
+            int bi = Convert.ToInt32(b);
+            int Ergebnis = 0;
+            if (Rechenzeichen == "+")
+            {
+              Ergebnis = si + bi;
+            }
+            if (Rechenzeichen == "-")
+            {
+                Ergebnis = si - bi;
+            }
+            if (Rechenzeichen == "*")
+            {
+                Ergebnis = si * bi;
+            }
+            if (Rechenzeichen == ":")
+            {
+                Ergebnis = si / bi;
+            }
+
+
+            string sErgebnis = Ergebnis.ToString();
+            
+            Console.WriteLine("Das Ergebnis ist: " + sErgebnis );
+            
+
+
+        }
+    }
+}
